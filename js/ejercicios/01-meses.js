a5451af35d0b3acb9d2a881fc7de4313c3e0fab7
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-03-24 14:54:05 -0300

    first commit

---
+++ js/ejercicios/01-meses.js
@@ -0,0 +1,22 @@
+// Creamos el array llamado 'meses' con los 12 meses del año
+const meses = [
+    "Enero", 
+    "Febrero", 
+    "Marzo", 
+    "Abril", 
+    "Mayo", 
+    "Junio", 
+    "Julio", 
+    "Agosto", 
+    "Septiembre", 
+    "Octubre", 
+    "Noviembre", 
+    "Diciembre"
+];
+document.writeln("<h1>Lista de Meses</h1>");
+document.writeln("<ul>"); // Empezamos una lista desordenada
+// Usamos un bucle for para recorrer el arreglo
+for (let i = 0; i < meses.length; i++) {
+    document.write("<li>" + meses[i] + "</li>");
+}
+document.write("</ul>"); // Cerramos la lista
