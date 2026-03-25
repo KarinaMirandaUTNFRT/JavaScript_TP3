c2403389b89f2ad80235dea19342bf7946b38dd4
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-03-25 18:00:03 -0300

    ejercicio 6 tp 3

--- js/ejercicios/05-mayusculasMinusculas.js
+++ js/ejercicios/05-mayusculasMinusculas.js
@@ -1,24 +1,21 @@
 function analizarTexto(cadena) {
   if (cadena === cadena.toUpperCase()) {
-    // Caso 1: Buscamos las mayusculas
+    // Caso 1: Buscamos si tiene todas mayusculas
     return "La cadena está formada solo por MAYÚSCULAS.";
   } else if (cadena === cadena.toLowerCase()) {
-    // Caso 2: Buscamos las minusculas
+    // Caso 2: Buscamos si tiene todas minusculas
     return "La cadena está formada solo por minúsculas.";
   } else {
     return "La cadena es una mezcla de mayúsculas y minúsculas.";
   }
 }
-// Ingreso del texto
-let textoUsuario = prompt("Ingresá una frase para analizar:");
-
-// Ejecutamos la función y mostramos el resultado
+ let textoUsuario = prompt("Ingresá una frase para analizar:");
 if (textoUsuario) {
   // Validamos que no esté vacío
   let resultado = analizarTexto(textoUsuario);
   document.writeln("<h2>Resultado del análisis:</h2>");
   document.writeln(
-    "<p>Texto ingresado: <strong>" + textoUsuario + "</strong></p>",
+    "<p>Texto ingresado:" + textoUsuario + "</p>",
   );
   document.writeln("<p>" + resultado + "</p>");
 } else {
